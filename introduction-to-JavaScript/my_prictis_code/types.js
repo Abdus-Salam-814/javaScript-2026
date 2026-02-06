16c8aca824d52279bd34cb7b00ea4ce848ee841d
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-02-06 12:17:06 +0600

    i finesd types price

---
+++ introduction-to-JavaScript/my_prictis_code/types.js
@@ -0,0 +1,22 @@
+// String 
+var name = "Sadar Uddin";
+var address = "Adar killa bandorban with some Katabon";
+
+var country = 'Bangladesh';
+
+
+// boolean
+var isRich = true;
+
+var passed =false;
+console.log(passed, "hi");
+
+console.log(typeof passed);
+console.log(typeof country);
+
+// number
+
+var price = 555;
+console.log(price)
+
+console.log(typeof price);
