564b55f78efc4e2939d59693bdf20be15554abc9
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-02-06 15:02:20 +0600

    i finesd types price

--- introduction-to-JavaScript/my_prictis_code/variable.js
+++ introduction-to-JavaScript/my_prictis_code/variable.js
@@ -1,33 +1,41 @@
-// VARIABLE ==> vary + able
-// 5 things to declare a variable
+// // VARIABLE ==> vary + able
+// // 5 things to declare a variable
 
 
 
-var weight = 38;
+// var weight = 38;
 
-var price = 38;
-var year = 1995;
-var money = 2005;
-money =5000;
+// var price = 38;
+// var year = 1995;
+// var money = 2005;
+// money =5000;
 
-var age;
+// var age;
 
-var sum = 'ttt' + 5;
+// var sum = 'ttt' + 5;
 
-let user = null;
-
-
+// let user = null;
 
 
-// console.log(price)
-
-console.log(money)
-
-console.log(age)
-
-console.log(sum)
-
-console.log(user)
 
 
-console.log(33)
+// // console.log(price)
+
+// console.log(money)
+
+// console.log(age)
+
+// console.log(sum)
+
+// console.log(user)
+
+
+// console.log(33)
+
+
+var age = 17;
+age = 35;
+age = 1000;
+age = 17;
+age = 1;
+console.log(age);
